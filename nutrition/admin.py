9d9b4c0c78fd0447de3f4f48ee29305676c47872
Dylan Boyd <dylanjboyd@me.com>
2019-02-15 10:58:27 +1300

    Add Consumption to Admin

--- nutrition/admin.py
+++ nutrition/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from nutrition.models import Food
+from nutrition.models import Food, Consumption
 
 admin.site.register(Food)
+
+
+class ConsumptionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'food', 'quantity', 'owner')
+
+
+admin.site.register(Consumption, ConsumptionAdmin)
