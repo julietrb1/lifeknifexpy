cd23802965305742bb0bf4ccbae7e6f44157d1ec
Dylan Boyd <dylanjboyd@me.com>
2019-02-18 12:12:01 +1300

    Make Goal cheat optional

--- goals/models.py
+++ goals/models.py
@@ -10,7 +10,8 @@ class Goal(models.Model):
     question = models.CharField(max_length=100, validators=[MinLengthValidator(10)])
     test = models.CharField(choices=TEST_CHOICES, max_length=10)
     frequency = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)])
-    cheat = models.PositiveSmallIntegerField(validators=[MinValueValidator(3), MaxValueValidator(14)])
+    cheat = models.PositiveSmallIntegerField(validators=[MinValueValidator(3), MaxValueValidator(14)], null=True,
+                                             blank=True)
     style = models.CharField(choices=STYLE_CHOICES, max_length=10)
     start_date = models.DateField()
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
